edc2fc83c30f3c5e5c6a1683224c46082c61cf24
Samuel Loup <loup@epsitec.ch>
2016-02-02 10:25:28 +0100

    Remove tapTouch injection

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 'use strict';
 import { IntlProvider } from 'react-intl';
-import injectTapEventPlugin from 'react-tap-event-plugin';
-// Needed for onTouchTap
-// Can go away when react 1.0 release
-// Check this repo:
-// https://github.com/zilverline/react-tap-event-plugin
-injectTapEventPlugin ();
 
 // API
 module.exports = {
