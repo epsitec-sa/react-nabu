7b2e18bf5edb0e3dcd0d45caa25f2ffa1ac3d924
Jonny Quarta <quarta@epsitec.ch>
2016-10-07 17:24:47 +0200

    Remove declaration of NabuProvider

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 'use strict';
 
-import {IntlProvider} from 'react-intl';
-
 // API
 module.exports = {
   NabuTranslator:      require ('./NabuTranslator.js'),
   NabuTranslatorPanel: require ('./NabuTranslatorPanel.js'),
-  T:                   require ('./NabuText.js'),
-  NabuProvider:        IntlProvider
+  T:                   require ('./NabuText.js')
 };
